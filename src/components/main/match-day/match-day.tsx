b18980f96e5ebae311349d44c27c186094457fd0
Wojtek Mikusek <w.mikusek@exlabs.pl>
2024-05-01 14:25:30 +0200

    Add match day empty state (#9)

--- src/components/main/match-day/match-day.tsx
+++ src/components/main/match-day/match-day.tsx
@@ -82,7 +82,7 @@ export const MatchDay = ({
           {headingLabel}
         </h3>
       </div>
-      <div className="relative w-full overflow-x-auto shadow-[rgba(0,_0,_0,_0.24)_0px_0px_8px] shadow-gray-600  sm:rounded-lg">
+      <div className="relative w-full overflow-x-auto shadow-[rgba(0,_0,_0,_0.24)_0px_0px_8px] shadow-gray-600 sm:rounded-lg">
         <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             <tr>
