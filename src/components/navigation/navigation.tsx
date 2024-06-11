135af4963afb6209335bf6a2b32dabf4cae99afc
kusiewicz <78431445+kusiewicz@users.noreply.github.com>
2024-06-11 16:32:18 +0200

    Add admin tab to mobile menu (#22)

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -29,7 +29,7 @@ const Navigation = async () => {
             Typer 2024
           </span>
         </a>
-        <MobileMenu matchDays={matchDays} />
+        <MobileMenu matchDays={matchDays} isAdmin={session?.user?.isAdmin} />
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-800">
             <NavLink to="/" label="Home" />
