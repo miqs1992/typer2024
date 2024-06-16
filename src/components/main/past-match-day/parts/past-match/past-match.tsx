a504856ed35877bd1beb7a942704cc6b8d243f66
Kamil Kusy <kamil.kusy111@gmail.com>
2024-06-16 15:51:36 +0200

    trying to fix match-day (test)

--- src/components/main/past-match-day/parts/past-match/past-match.tsx
+++ src/components/main/past-match-day/parts/past-match/past-match.tsx
@@ -3,7 +3,7 @@ import { getAllBetsForMatch } from "@/lib/actions/bet";
 import { PersistedMatch } from "@/modules/admin/round-match-management/match-management.service";
 
 export const PastMatch = async ({ match }: { match: PersistedMatch }) => {
-  const bets = await getAllBetsForMatch(match.id);
+  const bets = await getAllBetsForMatch(match?.id);
 
   return (
     <div className="relative my-[50px] mb-12 mt-0 w-full overflow-x-auto rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_0px_8px] shadow-gray-600">
@@ -21,9 +21,9 @@ export const PastMatch = async ({ match }: { match: PersistedMatch }) => {
               className="px-6 py-3 text-center text-[14px] font-bold"
             >
               <div className="flex items-center justify-center gap-2 whitespace-nowrap">
-                <FlagIcon country={match.firstTeam.flag} />
-                {`${match.firstTeamScore} - ${match.secondTeamScore}`}
-                <FlagIcon country={match.secondTeam.flag} />
+                <FlagIcon country={match?.firstTeam?.flag} />
+                {`${match?.firstTeamScore} - ${match?.secondTeamScore}`}
+                <FlagIcon country={match?.secondTeam?.flag} />
               </div>
             </th>
             <th scope="col" className="px-6 py-3 text-center">
@@ -33,10 +33,10 @@ export const PastMatch = async ({ match }: { match: PersistedMatch }) => {
         </thead>
         <tbody>
           {bets
-            .sort((a, b) => a.user.username.localeCompare(b.user.username))
+            .sort((a, b) => a?.user?.username.localeCompare(b?.user?.username))
             .map((bet) => {
-              const wonBet = bet.points > 0;
-              const isExactBet = bet.isExact;
+              const wonBet = bet?.points > 0;
+              const isExactBet = bet?.isExact;
               return (
                 <tr
                   className="border-b bg-white text-center dark:border-gray-700 dark:bg-gray-800"
@@ -48,21 +48,21 @@ export const PastMatch = async ({ match }: { match: PersistedMatch }) => {
                     colSpan={2}
                     style={{ width: "50%" }}
                   >
-                    {bet.user.username}
+                    {bet?.user?.username}
                   </th>
                   <td
                     className={`flex items-center justify-center whitespace-nowrap px-6 py-4 font-bold ${isExactBet ? "text-yellow-500" : ""}`}
                   >
                     <p
-                      className={`${bet.result.bonus ? "ml-[10px]" : "ml-[1px]"}`}
+                      className={`${bet?.result?.bonus ? "ml-[10px]" : "ml-[1px]"}`}
                     >
-                      {bet.result.firstTeamResult} -{" "}
-                      {bet.result.secondTeamResult}
-                      {bet.result.bonus ? " *" : ""}
+                      {bet?.result?.firstTeamResult} -{" "}
+                      {bet?.result?.secondTeamResult}
+                      {bet?.result?.bonus ? " *" : ""}
                     </p>
                   </td>
                   <td className="whitespace-nowrap px-6 py-4 font-bold">
-                    {bet.points.toFixed(2)}
+                    {bet?.points?.toFixed(2)}
                   </td>
                 </tr>
               );
